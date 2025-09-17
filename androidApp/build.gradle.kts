9a9e1d06eb03940769dfdbfb5a1aa286c810d313
Sabina Leitoiu <sabina.leitoiu@masabi.com>
2025-09-17 22:43:47 +0100

    Update dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -32,9 +32,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+//    kotlinJvmToolchain(17)
 }
 
 dependencies {
