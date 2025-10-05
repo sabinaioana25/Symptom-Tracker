9dffd30dbc6624d1528c97f237e8d4b4e0924c2c
Sabina Leitoiu <sabinaleitoiu91@gmail.com>
2025-10-05 22:31:20 +0100

    Delete SymptomTracker  directory

--- SymptomTracker /shared/src/commonMain/kotlin/com/example/symptomtracker/domain/usecase/NoteUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.symptomtracker.domain.usecase
-
-import com.example.symptomtracker.domain.model.Note
-import com.example.symptomtracker.domain.repository.NoteRepositoryImpl
-
-class NoteUseCase(private val repositoryImpl: NoteRepositoryImpl) {
-  suspend operator fun invoke(note: Note) = repositoryImpl.insert(note)
-}
